283865d9e83058b33cd0a0c1d80b703edf1505f9
andrei <andrei29.cervantes@gmail.com>
2026-06-27 23:23:53 +0800

    setup: add docker configuration for postgresql and fix db pool export

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 
 // Create connection pool with env
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  user: process.env.DB_USER || "chatappapidb",
+  host: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "chat_app",
-  password: process.env.DB_PASSWORD || "postgres",
-  port: process.env.DB_PORT || 5432,
+  password: process.env.DB_PASSWORD || "chatappapipassword",
+  port: process.env.DB_PORT || 5433,
 });
 
-module.exports = pool;
+module.exports = { pool };
